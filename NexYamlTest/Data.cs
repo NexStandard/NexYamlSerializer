aa39eaff932579e00a129c197d4ebb41215ad042
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-01 18:47:34 +0100

    add tests

---
+++ NexYamlTest/Data.cs
@@ -0,0 +1,50 @@
+﻿using Stride.Core;
+namespace NexYamlTest;
+[DataContract]
+public record EmptyRecord
+{
+}
+[DataContract]
+public class EmptyClass
+{
+    public int ID { get; set; }
+    public override bool Equals(object obj)
+    {
+        // Check if the object is null or of a different type
+        if (obj == null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        // Convert the object to the same type as this instance
+        var other = (EmptyClass)obj;
+
+        // Compare the ID property for equality
+        return ID == other.ID;
+    }
+}
+[DataContract]
+public struct EmptyStruct
+{
+    public int ID { get; set; }
+    public override bool Equals(object obj)
+    {
+        // Check if the object is null or of a different type
+        if (obj is not EmptyStruct)
+        {
+            return false;
+        }
+
+        // Convert the object to the same type as this instance
+        EmptyStruct other = (EmptyStruct)obj;
+
+        // Compare the fields or properties for equality
+        return ID == other.ID;
+    }
+
+    // Optionally, you may want to override GetHashCode as well when you override Equals
+    public override int GetHashCode()
+    {
+        return ID.GetHashCode();
+    }
+}
---
+++ NexYamlTest/SimpleClasses.cs
@@ -0,0 +1,71 @@
+﻿using NexVYaml;
+using NexVYaml.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexYamlTest;
+
+
+public class SimpleClasses
+{
+    public void Setup()
+    {
+        NexYamlSerializerRegistry.Init();
+    }
+    /// <summary>
+    /// Tests the serialization and deserialization of an <see cref="EmptyRecord"/>.
+    /// </summary>
+    [Fact]
+    public void EmptyRecord()
+    {
+        // Arrange
+        Setup();
+
+        // Act
+        var originalData = new EmptyRecord();
+        var serializedData = YamlSerializer.SerializeToString(originalData);
+        var deserializedData = YamlSerializer.Deserialize<EmptyRecord>(serializedData);
+
+        // Assert
+        Assert.Equal(originalData, deserializedData);
+    }
+    /// <summary>
+    /// Tests the serialization and deserialization of an <see cref="EmptyClass"/>.
+    /// </summary>
+    [Fact]
+    public void EmptyClass()
+    {
+        // Arrange
+        Setup();
+
+        // Act
+        var originalData = new EmptyClass();
+        var serializedData = YamlSerializer.SerializeToString(originalData);
+        var deserializedData = YamlSerializer.Deserialize<EmptyClass>(serializedData);
+
+        // Assert
+        Assert.Equal(originalData, deserializedData);
+    }
+
+    /// <summary>
+    /// Tests the serialization and deserialization of an EmptyStruct.
+    /// </summary>
+    [Fact]
+    public void EmptyStruct()
+    {
+        // Arrange
+        Setup();
+
+        // Act
+        var originalData = new EmptyStruct();
+        var serializedData = YamlSerializer.SerializeToString(originalData);
+        var deserializedData = YamlSerializer.Deserialize<EmptyStruct>(serializedData);
+
+        // Assert
+        Assert.Equal(originalData, deserializedData);
+    }
+}
