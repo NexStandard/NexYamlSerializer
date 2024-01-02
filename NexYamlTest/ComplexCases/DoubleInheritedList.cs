ae0c56907bd85b5c1bcadf1e46dad2c34ed3dff0
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-02 20:35:57 +0100

    add collection test

---
+++ NexYamlTest/ComplexCases/DoubleInheritedList.cs
@@ -0,0 +1,53 @@
+﻿using Stride.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexYamlTest.ComplexCases;
+[DataContract]
+internal class DoubleInheritedList : ListWithOnEditCallback<int>
+{
+}
+[DataContract(Inherited = true)]
+public class ListWithOnEditCallback<T> : List<T>
+{
+    public Action? OnEditCallBack { get; internal set; }
+
+    public new void Add(T item)
+    {
+        base.Add(item);
+        OnEditCallBack?.Invoke();
+    }
+    public new void Remove(T item)
+    {
+        base.Remove(item);
+        OnEditCallBack?.Invoke();
+    }
+    public new void RemoveAll(Predicate<T> match)
+    {
+        base.RemoveAll(match);
+        OnEditCallBack?.Invoke();
+    }
+    public new void RemoveAt(int index)
+    {
+        base.RemoveAt(index);
+        OnEditCallBack?.Invoke();
+    }
+    public new void RemoveRange(int index, int count)
+    {
+        base.RemoveRange(index, count);
+        OnEditCallBack?.Invoke();
+    }
+    public new void AddRange(IEnumerable<T> collection)
+    {
+        base.AddRange(collection);
+        OnEditCallBack?.Invoke();
+    }
+    public new void Clear()
+    {
+        base.Clear();
+        OnEditCallBack?.Invoke();
+    }
+}
---
+++ NexYamlTest/ComplexTests.cs
@@ -0,0 +1,36 @@
+﻿using NexVYaml;
+using NexVYaml.Serialization;
+using NexYamlTest.ComplexCases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexYamlTest;
+public class ComplexTests
+{
+    private void Setup()
+    {
+        NexYamlSerializerRegistry.Init();
+    }
+    [Fact(Skip = "Unimplemented ICollection handling")]
+    public void DoubleInheritedListTest()
+    {
+        Setup();
+        var list = new DoubleInheritedList()
+        {
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
+        };
+
+        var s = YamlSerializer.SerializeToString(list);
+        var deserialized = YamlSerializer.Deserialize<DoubleInheritedList>(s);
+        Assert.Null(s);
+        Assert.Equal(list.Count,deserialized.Count);
+        for ( var i = 0; i < list.Count; i++ )
+        {
+            Assert.Equal(list[i], deserialized[i] );
+        }
+    }
+}
