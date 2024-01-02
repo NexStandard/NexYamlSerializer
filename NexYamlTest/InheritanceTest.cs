537716ce27c10c657e052f0fde1a32ae5dcaa03e
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-02 20:35:44 +0100

    add inheritance test #1

---
+++ NexYamlTest/InheritanceTest.cs
@@ -0,0 +1,31 @@
+﻿using NexVYaml;
+using NexVYaml.Serialization;
+using NexYamlTest.ComplexCases;
+using NexYamlTest.SimpleClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexYamlTest;
+public class InheritanceTest
+{
+    private void Setup() => NexYamlSerializerRegistry.Init();
+    [Fact]
+    public void Inheritance_NormalClass_NoOverride()
+    {
+        Setup();
+        var inherited = new InheritedNormalClass()
+        {
+            Test = 100,
+            Name = "Bob"
+        };
+        var s = YamlSerializer.SerializeToString(inherited);
+        var deserialized = YamlSerializer.Deserialize<InheritedNormalClass>(s);
+        Assert.NotNull(deserialized);
+        Assert.Equal(100, deserialized.Test);
+        Assert.Equal("Bob", deserialized.Name);
+    }
+}
---
+++ NexYamlTest/SimpleClasses/InheritedNormalClass.cs
@@ -0,0 +1,13 @@
+﻿using Stride.Core;
+
+namespace NexYamlTest.SimpleClasses;
+[DataContract]
+internal class InheritedNormalClass : BaseClass
+{
+    public int Test {  get; set; }
+}
+[DataContract]
+internal class BaseClass
+{
+    public string Name { get; set; }
+}
