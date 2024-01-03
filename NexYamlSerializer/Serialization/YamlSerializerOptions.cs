fa6cb1bddaf77951f5c5ed92b3f62e755bce3769
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-03 02:38:52 +0100

    update securemode docs

--- NexYamlSerializer/Serialization/YamlSerializerOptions.cs
+++ NexYamlSerializer/Serialization/YamlSerializerOptions.cs
@@ -5,21 +5,15 @@ namespace NexVYaml.Serialization
 {
     public class YamlSerializerOptions
     {
-        public static YamlSerializerOptions Standard => new()
-        {
-            Resolver = NexYamlSerializerRegistry.Instance,
-            SecureMode = false
-        };
+        public static YamlSerializerOptions Standard => new();
 
-        public IYamlFormatterResolver Resolver { get; set; } = null!;
+        public IYamlFormatterResolver Resolver { get; set; } = NexYamlSerializerRegistry.Instance;
         public YamlEmitOptions EmitOptions { get; set; } = new();
         /// <summary>
-        /// Experimental:
-        /// Uses <see cref="SecureRedirectFormatter{T}"/>.
-        /// Limits Serialization/Deserialization to known Formatters, which are non generic.
-        /// Tags won't have an effect.
+        /// Limits <see cref="RedirectFormatter{T}"/> Serialization/Deserialization to known Basic Formatters.
+        /// Tags won't have an effect but will be still written to the file.
+        /// Interface/Abstract Serialization/Deserialization is blocked.
         /// </summary>
         public bool SecureMode { get; set; } = false;
-        public bool EnableAliasForDeserialization { get; set; } = true;
     }
 }
