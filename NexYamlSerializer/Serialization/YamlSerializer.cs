676cab32b61ea198ff9df79744c1816498940ba2
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-04 01:21:41 +0100

    add docs

--- NexYamlSerializer/Serialization/YamlSerializer.cs
+++ NexYamlSerializer/Serialization/YamlSerializer.cs
@@ -70,23 +70,55 @@ namespace NexVYaml
             }
         }
 
-
+        /// <summary>
+        /// Serializes the specified object of type <typeparamref name="T"/> or any derived class/interface of type <typeparamref name="T"/> to YAML format 
+        /// and writes it to the given <paramref name="stream"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be serialized, or any type derived from <typeparamref name="T"/>.</typeparam>
+        /// <param name="value">The object to be serialized.</param>
+        /// <param name="stream">The stream to which the YAML representation will be written.</param>
+        /// <param name="options">Optional settings for customizing the YAML serialization process.</param>
         public static void Serialize<T>(T value, Stream stream,YamlSerializerOptions? options = null)
         {
             stream.Write(Serialize(value, options).Span);
         }
 
+        /// <summary>
+        /// Asynchronously serializes the specified object of type <typeparamref name="T"/> or any derived class/interface of type <typeparamref name="T"/> to YAML format 
+        /// and writes it to the given <paramref name="stream"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be serialized, or any type derived from <typeparamref name="T"/>.</typeparam>
+        /// <param name="value">The object to be serialized.</param>
+        /// <param name="stream">The stream to which the YAML representation will be written asynchronously.</param>
+        /// <param name="options">Optional settings for customizing the YAML serialization process.</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
         public async static Task SerializeAsync<T>(T value, Stream stream, YamlSerializerOptions? options = null)
         {
             await stream.WriteAsync(Serialize(value, options));
         }
 
+        /// <summary>
+        /// Serializes the specified object of type <typeparamref name="T"/> or any derived class/interface of type <typeparamref name="T"/> to YAML format 
+        /// and writes it to the provided <paramref name="writer"/> using an <see cref="IBufferWriter{T}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be serialized, or any type derived from <typeparamref name="T"/>.</typeparam>
+        /// <param name="writer">The buffer writer used to write the YAML content.</param>
+        /// <param name="value">The object to be serialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML serialization process.</param>
         public static void Serialize<T>(IBufferWriter<byte> writer, T value, YamlSerializerOptions? options = null)
         {
             var emitter = new Utf8YamlEmitter(writer);
             Serialize(ref emitter, value, options);
         }
 
+        /// <summary>
+        /// Serializes the specified object of type <typeparamref name="T"/> or any derived class/interface of type <typeparamref name="T"/> to YAML format 
+        /// using the provided <paramref name="emitter"/> and <paramref name="options"/> and disposes the emitter afterward.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be serialized, or any type derived from <typeparamref name="T"/>.</typeparam>
+        /// <param name="emitter">The Utf8YamlEmitter used for serializing the YAML content.</param>
+        /// <param name="value">The object to be serialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML serialization process.</param>
         public static void Serialize<T>(ref Utf8YamlEmitter emitter, T value, YamlSerializerOptions? options = null)
         {
             try
@@ -103,27 +135,65 @@ namespace NexVYaml
             }
         }
 
+        /// <summary>
+        /// Serializes the specified object of type <typeparamref name="T"/> or any derived class/interface of type <typeparamref name="T"/> to YAML format 
+        /// and returns the serialized content as a UTF-8 encoded string.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be serialized, or any type derived from <typeparamref name="T"/>.</typeparam>
+        /// <param name="value">The object to be serialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML serialization process.</param>
+        /// <returns>A UTF-8 encoded string representing the YAML serialization of the object.</returns>
         public static string SerializeToString<T>(T value, YamlSerializerOptions? options = null)
         {
             var utf8Bytes = Serialize(value, options);
             return StringEncoding.Utf8.GetString(utf8Bytes.Span);
         }
 
+        /// <summary>
+        /// Deserializes the YAML content from the specified <paramref name="memory"/> to an object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be deserialized.</typeparam>
+        /// <param name="memory">The ReadOnlyMemory containing the YAML content to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>An object of type <typeparamref name="T"/> representing the deserialized YAML content.</returns>
         public static T Deserialize<T>(ReadOnlyMemory<byte> memory, YamlSerializerOptions? options = null)
         {
             var parser = YamlParser.FromSequence(new ReadOnlySequence<byte>(memory));
             return Deserialize<T>(ref parser, options);
         }
+
+        /// <summary>
+        /// Deserializes the YAML content from the specified <paramref name="yaml"/> string to an object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be deserialized.</typeparam>
+        /// <param name="yaml">The string containing the YAML content to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>An object of type <typeparamref name="T"/> representing the deserialized YAML content.</returns>
         public static T Deserialize<T>(string yaml, YamlSerializerOptions? options = null)
         {
             return Deserialize<T>(Encoding.UTF8.GetBytes(yaml), options);
         }
+
+        /// <summary>
+        /// Deserializes the YAML content from the specified <paramref name="sequence"/> to an object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be deserialized.</typeparam>
+        /// <param name="sequence">The ReadOnlySequence containing the YAML content to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>An object of type <typeparamref name="T"/> representing the deserialized YAML content.</returns>
         public static T Deserialize<T>(in ReadOnlySequence<byte> sequence, YamlSerializerOptions? options = null)
         {
             var parser = YamlParser.FromSequence(sequence);
             return Deserialize<T>(ref parser, options);
         }
 
+        /// <summary>
+        /// Asynchronously deserializes the YAML content from the specified <paramref name="stream"/> to an object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be deserialized.</typeparam>
+        /// <param name="stream">The Stream containing the YAML content to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>A ValueTask representing the asynchronous operation, with the result being an object of type <typeparamref name="T"/> representing the deserialized YAML content.</returns>
         public static async ValueTask<T> DeserializeAsync<T>(Stream stream, YamlSerializerOptions? options = null)
         {
             var byteSequenceBuilder = await StreamHelper.ReadAsSequenceAsync(stream);
@@ -138,6 +208,13 @@ namespace NexVYaml
             }
         }
 
+        /// <summary>
+        /// Deserializes the YAML content using the provided <paramref name="parser"/> to an object of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to be deserialized.</typeparam>
+        /// <param name="parser">The YamlParser used for deserializing the YAML content.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>An object of type <typeparamref name="T"/> representing the deserialized YAML content.</returns>
         public static T Deserialize<T>(ref YamlParser parser, YamlSerializerOptions? options = null)
         {
             try
@@ -156,6 +233,14 @@ namespace NexVYaml
             }
         }
 
+        /// <summary>
+        /// Asynchronously deserializes multiple YAML documents from the specified <paramref name="stream"/> to objects of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects to be deserialized.</typeparam>
+        /// <param name="stream">The Stream containing the YAML content with multiple documents to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>A ValueTask representing the asynchronous operation, with the result being an IEnumerable of objects of type <typeparamref name="T"/> representing the deserialized YAML documents.</returns>
+        [Obsolete("The state of this method's functionality is uncertain. Use alternative deserialization methods.")]
         public static async ValueTask<IEnumerable<T>> DeserializeMultipleDocumentsAsync<T>(Stream stream, YamlSerializerOptions? options = null)
         {
             var byteSequenceBuilder = await StreamHelper.ReadAsSequenceAsync(stream);
@@ -170,18 +255,28 @@ namespace NexVYaml
             }
         }
 
+        /// <summary>
+        /// Deserializes multiple YAML documents from the specified <paramref name="memory"/> to objects of type <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the objects to be deserialized.</typeparam>
+        /// <param name="memory">The ReadOnlyMemory containing the YAML content with multiple documents to be deserialized.</param>
+        /// <param name="options">Optional settings for customizing the YAML deserialization process.</param>
+        /// <returns>An IEnumerable of objects of type <typeparamref name="T"/> representing the deserialized YAML documents.</returns>
+        [Obsolete("Unknown state if it's working or not.")]
         public static IEnumerable<T> DeserializeMultipleDocuments<T>(ReadOnlyMemory<byte> memory, YamlSerializerOptions? options = null)
         {
             var parser = YamlParser.FromSequence(new ReadOnlySequence<byte>(memory));
             return DeserializeMultipleDocuments<T>(ref parser, options);
         }
 
+        [Obsolete("Unknown state if it's working or not.")]
         public static IEnumerable<T> DeserializeMultipleDocuments<T>(in ReadOnlySequence<byte> sequence, YamlSerializerOptions? options = null)
         {
             var parser = YamlParser.FromSequence(sequence);
             return DeserializeMultipleDocuments<T>(ref parser, options);
         }
 
+        [Obsolete("Unknown state if it's working or not.")]
         public static IEnumerable<T> DeserializeMultipleDocuments<T>(ref YamlParser parser, YamlSerializerOptions? options = null)
         {
             try
