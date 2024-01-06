3f0efcff01dc1663978afc8c2ab8eb30cc309510
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-06 17:38:29 +0100

    add const field test

---
+++ NexYamlTest/ConstFieldTest.cs
@@ -0,0 +1,29 @@
+﻿using NexVYaml;
+using NexYamlTest.SimpleClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexYamlTest;
+public class ConstFieldTest
+{
+    private void Setup()
+    {
+        NexYamlSerializerRegistry.Init();
+    }
+    [Fact]
+    public void ConstField()
+    {
+        Setup();
+        var aliased = new ClassWithConstField()
+        {
+            Normal = 1,
+        };
+        var s = YamlSerializer.SerializeToString(aliased);
+        var deserialized = YamlSerializer.Deserialize<ClassWithConstField>(s);
+        Assert.Equal(aliased.Normal, deserialized.Normal);
+    }
+}
---
+++ NexYamlTest/SimpleClasses/ClassWithConstField.cs
@@ -0,0 +1,10 @@
+﻿
+using Stride.Core;
+
+namespace NexYamlTest.SimpleClasses;
+[DataContract]
+internal class ClassWithConstField
+{
+    public const int Constant = 10;
+    public int Normal = 0;
+}
