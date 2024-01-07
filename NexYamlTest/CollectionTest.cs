0ea1a74c5e3beabe0a504e1e34029be760c2235f
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-07 19:48:20 +0100

    add collections test

---
+++ NexYamlTest/CollectionTest.cs
@@ -0,0 +1,76 @@
+﻿using NexVYaml;
+using NexYamlTest.SimpleClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace NexYamlTest;
+public class CollectionTest
+{
+    [Fact]
+    public void Collection()
+    {
+        // Creating test data
+        TempData data1 = new TempData()
+        {
+            name = "John",
+            id = 1,
+        };
+        TempData data2 = new TempData()
+        {
+            name = "Alice",
+            id = 2,
+        };
+
+        // Creating an object of the Collections class
+        Collections testCollections = new Collections();
+
+        // Filling the keyValuePairs dictionary
+        testCollections.keyValuePairs.Add("Key1", data1);
+        testCollections.keyValuePairs.Add("Key2", data2);
+
+        // Filling the ComplexDictionary dictionary
+        testCollections.ComplexDictionary.Add(data1, data2);
+        testCollections.ComplexDictionary.Add(data2, data1);
+
+        // Filling the strings list
+        testCollections.strings.Add("String1");
+        testCollections.strings.Add("String2");
+
+        // Filling the values list
+        testCollections.values.Add(data1);
+        testCollections.values.Add(data2);
+
+        NexYamlSerializerRegistry.Init();
+        var s = YamlSerializer.Serialize(testCollections);
+        var d = YamlSerializer.Deserialize<Collections>(s);
+        // Asserts
+        Assert.NotNull(d);
+        Assert.NotNull(d.keyValuePairs);
+        Assert.Equal(2, d.keyValuePairs.Count);
+        Assert.True(d.keyValuePairs.ContainsKey("Key1"));
+        Assert.True(d.keyValuePairs.ContainsKey("Key2"));
+        Assert.Equal(data1, d.keyValuePairs["Key1"]);
+        Assert.Equal(data2, d.keyValuePairs["Key2"]);
+
+        Assert.NotNull(d.ComplexDictionary);
+        Assert.Equal(2, d.ComplexDictionary.Count);
+        Assert.True(d.ComplexDictionary.ContainsKey(data1));
+        Assert.True(d.ComplexDictionary.ContainsKey(data2));
+        Assert.Equal(data2, d.ComplexDictionary[data1]);
+        Assert.Equal(data1, d.ComplexDictionary[data2]);
+
+        Assert.NotNull(d.strings);
+        Assert.Equal(2, d.strings.Count);
+        Assert.Contains("String1", d.strings);
+        Assert.Contains("String2", d.strings);
+
+        Assert.NotNull(d.values);
+        Assert.Equal(2, d.values.Count);
+        Assert.Contains(data1, d.values);
+        Assert.Contains(data2, d.values);
+    }
+}
---
+++ NexYamlTest/SimpleClasses/Collections.cs
@@ -0,0 +1,22 @@
+﻿using Stride.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NexYamlTest.SimpleClasses;
+[DataContract]
+internal class Collections
+{
+    public Dictionary<string, TempData> keyValuePairs = [];
+    public Dictionary<TempData, TempData> ComplexDictionary = [];
+    public List<string> strings = [];
+    public List<TempData> values = [];
+}
+[DataContract]
+public record TempData
+{
+    public string name { get; set; }
+    public int id { get; set; }
+}
