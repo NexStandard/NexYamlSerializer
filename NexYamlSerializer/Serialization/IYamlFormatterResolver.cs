fcf7c5843a5cfc94f547edb091b06c25e7c6ba17
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-01-13 08:20:17 +0100

    remove redirect formatter

--- NexYamlSerializer/Serialization/IYamlFormatterResolver.cs
+++ NexYamlSerializer/Serialization/IYamlFormatterResolver.cs
@@ -24,51 +24,9 @@ namespace NexVYaml.Serialization
 
     public static class YamlFormatterResolverExtensions
     {
-        static Type NullableFormatter = typeof(NullableFormatter<>);
         public static bool IsNullable(Type value, out Type underlyingType)
         {
             return (underlyingType = Nullable.GetUnderlyingType(value)) != null;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static IYamlFormatter<T> GetFormatterWithVerify<T>(this IYamlFormatterResolver resolver)
-        {
-            IYamlFormatter<T>? formatter;
-            try
-            {
-                var type = typeof(T);
-                if (IsNullable(type, out var underlyingType))
-                {
-                        var genericFilledFormatter = NullableFormatter.MakeGenericType(underlyingType);
-
-                        formatter = (IYamlFormatter<T>)Activator.CreateInstance(genericFilledFormatter, args:  resolver.GetFormatter(underlyingType));
-                }
-                else
-                if (type.IsInterface || type.IsAbstract || type.IsGenericType)
-                {
-                    formatter = new RedirectFormatter<T>();
-                }
-                else
-                {
-                    formatter = resolver.GetFormatter<T>();
-                }
-            }
-            catch (TypeInitializationException ex)
-            {
-                ExceptionDispatchInfo.Capture(ex.InnerException ?? ex).Throw();
-                return default!; // not reachable
-            }
-
-            if (formatter != null)
-            {
-                return formatter;
-            }
-            Throw(typeof(T), resolver);
-            return default!; // not reachable
-        }
-        
-        static void Throw(Type t, IYamlFormatterResolver resolver)
-        {
-            throw new YamlSerializerException(t.FullName + $"{t} is not registered in resolver: {resolver.GetType()}");
         }
     }
 }
