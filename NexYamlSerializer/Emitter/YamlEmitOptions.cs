5ddd047623dba1f981e0c750ba08e97fe1e8cb24
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-04-18 00:26:59 +0200

    remove unused

--- NexYamlSerializer/Emitter/YamlEmitOptions.cs
+++ NexYamlSerializer/Emitter/YamlEmitOptions.cs
@@ -1,24 +1,6 @@
 #nullable enable
 namespace NexVYaml.Emitter;
 
-public enum SequenceStyle
-{
-    Block,
-    Flow,
-}
-public enum YamlStyle
-{
-    BlockMapping,
-    FlowMapping,
-    BlockSequence,
-    FlowSequence
-}
-public enum MappingStyle
-{
-    Block,
-    Flow,
-}
-
 public class YamlEmitOptions
 {
     public static YamlEmitOptions Default => new();
