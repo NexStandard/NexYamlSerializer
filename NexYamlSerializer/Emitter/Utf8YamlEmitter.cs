b6f320a74e891030d01d1de89304fd9f02606568
IXLLEGACYIXL <ixllegacy123@outlook.com>
2024-05-30 16:27:32 +0200

    feat: simplify utf8 yaml emitter

--- NexYamlSerializer/Emitter/Utf8YamlEmitter.cs
+++ NexYamlSerializer/Emitter/Utf8YamlEmitter.cs
@@ -20,7 +20,7 @@ enum EmitState
     FlowMappingKey,
     FlowMappingValue,
 }
-sealed partial class Utf8YamlEmitter : IDisposable
+sealed class Utf8YamlEmitter : IDisposable
 {
     public int CurrentIndentLevel => IndentationManager.CurrentIndentLevel;
     internal ExpandBuffer<EmitState> StateStack { get; }
@@ -190,4 +190,145 @@ sealed partial class Utf8YamlEmitter : IDisposable
         }
         Writer.Advance(offset);
     }
+    #region Writes
+    byte[] whiteSpaces =
+[
+        (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ',
+            (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ',
+            (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ',
+            (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ', (byte)' ',
+    ];
+
+    public void WriteScalar(ReadOnlySpan<byte> value)
+    {
+        var offset = 0;
+        var output = Writer.GetSpan(CalculateMaxScalarBufferLength(value.Length));
+
+        BeginScalar(output, ref offset);
+        value.CopyTo(output[offset..]);
+        offset += value.Length;
+        EndScalar(output, ref offset);
+    }
+
+    internal void WriteIndent(Span<byte> output, ref int offset, int forceWidth = -1)
+    {
+        int length;
+        if (forceWidth > -1)
+        {
+            if (forceWidth <= 0)
+                return;
+            length = forceWidth;
+        }
+        else if (CurrentIndentLevel > 0)
+        {
+            length = CurrentIndentLevel * IndentWidth;
+        }
+        else
+        {
+            return;
+        }
+
+        if (length > whiteSpaces.Length)
+        {
+            whiteSpaces = Enumerable.Repeat(YamlCodes.Space, length * 2).ToArray();
+        }
+        whiteSpaces.AsSpan(0, length).CopyTo(output[offset..]);
+        offset += length;
+    }
+
+    internal void WriteRaw(byte value)
+    {
+        var output = Writer.GetSpan(1);
+        output[0] = value;
+        Writer.Advance(1);
+    }
+
+    internal void WriteRaw(ReadOnlySpan<byte> value, bool indent, bool lineBreak)
+    {
+        var length = value.Length +
+                     (indent ? CurrentIndentLevel * IndentWidth : 0) +
+                     (lineBreak ? 1 : 0);
+
+        var offset = 0;
+        var output = Writer.GetSpan(length);
+        if (indent)
+        {
+            WriteIndent(output, ref offset);
+        }
+        value.CopyTo(output[offset..]);
+        if (lineBreak)
+        {
+            output[length - 1] = YamlCodes.Lf;
+        }
+        Writer.Advance(length);
+    }
+
+    internal void WriteRaw(ReadOnlySpan<byte> value1, ReadOnlySpan<byte> value2, bool indent, bool lineBreak)
+    {
+        var length = value1.Length + value2.Length +
+                     (indent ? CurrentIndentLevel * IndentWidth : 0) +
+                     (lineBreak ? 1 : 0);
+        var offset = 0;
+        var output = Writer.GetSpan(length);
+        if (indent)
+        {
+            WriteIndent(output, ref offset);
+        }
+
+        value1.CopyTo(output[offset..]);
+        offset += value1.Length;
+
+        value2.CopyTo(output[offset..]);
+        if (lineBreak)
+        {
+            output[length - 1] = YamlCodes.Lf;
+        }
+        Writer.Advance(length);
+    }
+
+    internal void WriteBlockSequenceEntryHeader()
+    {
+        if (IsFirstElement)
+        {
+            switch (StateStack.Previous)
+            {
+                case EmitState.BlockSequenceEntry:
+                    WriteRaw(YamlCodes.Lf);
+                    IndentationManager.IncreaseIndent();
+                    break;
+                case EmitState.BlockMappingValue:
+                    WriteRaw(YamlCodes.Lf);
+                    break;
+            }
+        }
+        WriteRaw(EmitCodes.BlockSequenceEntryHeader, true, false);
+    }
+
+    public int CalculateMaxScalarBufferLength(int length)
+    {
+        var around = ((CurrentIndentLevel + 1) * IndentWidth) + 3;
+        if (tagStack.Length > 0)
+        {
+            length += StringEncoding.Utf8.GetMaxByteCount(tagStack.Peek().Length) + around;
+        }
+        return length;
+    }
+
+    internal void PushState(EmitState state)
+    {
+        StateStack.Add(state);
+        elementCountStack.Add(currentElementCount);
+        currentElementCount = 0;
+    }
+
+    internal void PopState()
+    {
+        StateStack.Pop();
+        currentElementCount = elementCountStack.Length > 0 ? elementCountStack.Pop() : 0;
+    }
+    public void Tag(ref string value)
+    {
+        tagStack.Add(value);
+    }
+    #endregion
 }
