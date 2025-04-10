5a135260418ca6ccb2f3fddda37688dc1d3f0715
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-04-10 05:43:58 +0200

    feat: improve docs

--- NexYaml/Plugins/IResolvePlugin.cs
+++ NexYaml/Plugins/IResolvePlugin.cs
@@ -4,17 +4,22 @@ using Stride.Core;
 namespace NexYaml.Plugins;
 /// <summary>
 /// Provides the <see cref="IYamlWriter"/> with Syntax options to handle special cases during type resolution
+/// Plugins resolve before any redirection, any plugin will be passed as long as the value is not consumed
 /// </summary>
 public interface IResolvePlugin
 {
     /// <summary>
-    /// Investigates if the <paramref name="value"/> or the typeof <typeparamref name="T"/> needs special handling
-    /// which can't be resolved through normal serializers.
+    /// Determines whether the given <paramref name="value"/> of type <typeparamref name="T"/> 
+    /// requires special serialization handling before the standard serializers.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="stream">The stream to handle the case on</param>
-    /// <param name="value">The value to handle</param>
-    /// <returns>True if the value is consumed, false if the value couldn't be handled</returns>
+    /// <typeparam name="T">The type of the value to serialize.</typeparam>
+    /// <param name="stream">The YAML writer to write the value to.</param>
+    /// <param name="value">The value to be written.</param>
+    /// <param name="provider">Specifies the desired YAML data style.</param>
+    /// <returns>
+    /// True if the value was successfully handled and written by this method; 
+    /// false if the value was not processed and should be handled by the default serializers.
+    /// </returns>
     bool Write<T>(IYamlWriter stream, T value, DataStyle provider);
     bool Read<T>(IYamlReader stream, ref T value, ref ParseResult result);
 }
