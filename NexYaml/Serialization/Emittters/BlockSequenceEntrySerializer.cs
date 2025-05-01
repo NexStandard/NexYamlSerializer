05426dd89499a11ecc24a44bbfe11bbdfdba6e7b
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-05-01 21:41:55 +0200

    feat: simplify block sequence

--- NexYaml/Serialization/Emittters/BlockSequenceEntrySerializer.cs
+++ NexYaml/Serialization/Emittters/BlockSequenceEntrySerializer.cs
@@ -10,25 +10,7 @@ internal class BlockSequenceEntrySerializer : IEmitter
     public override EmitState State { get; } = EmitState.BlockSequenceEntry;
 
     public override void Begin()
-    {
-        switch (machine.Current.State)
-        {
-            case EmitState.BlockSequenceEntry:
-                WriteBlockSequenceEntryHeader();
-                break;
-
-            case EmitState.FlowSequenceEntry:
-                throw new YamlException(
-                    "To start block-sequence in the flow-sequence is not supported.");
-            case EmitState.FlowMappingKey:
-                throw new YamlException(
-                    "To start block-sequence in the flow mapping key is not supported.");
-            case EmitState.BlockMappingKey or EmitState.BlockMappingSecondaryKey:
-                throw new YamlException(
-                    "To start block-sequence in the mapping key is not supported.");
-        }
-
-        
+    { 
         if (machine.TryGetTag(out var tag))
         {
             WriteRaw(tag);
@@ -37,6 +19,7 @@ internal class BlockSequenceEntrySerializer : IEmitter
         switch (machine.Current.State)
         {
             case EmitState.BlockSequenceEntry:
+                WriteBlockSequenceEntryHeader();
                 machine.IndentationManager.IncreaseIndent();
                 WriteNewLine();
                 break;
