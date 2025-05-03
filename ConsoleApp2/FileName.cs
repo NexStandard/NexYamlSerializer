6280062f3f8ff41a0992968b7b597f9a40c27e7f
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-05-03 23:35:11 +0200

    feat: add benchmark

--- ConsoleApp2/FileName.cs
+++ ConsoleApp2/FileName.cs
@@ -1,37 +1,62 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Engines;
 using NexYaml;
+using NexYaml.Plugins;
 using NexYaml.Serialization;
 using NexYaml.Serializers;
+using SharpFont;
 using Stride.Core;
+using System.IO;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 namespace Test;
-[MemoryDiagnoser(false)]
+[MemoryDiagnoser()]
 public class Benchmarker
 {
+    IYamlSerializerResolver resolver;
+    Collections values = new Collections();
+    MemoryStream memoryStream;
+    YamlWriter writer;
+    StreamWriter s;
+    [GlobalSetup]
+    public void Setup()
+    {
+        resolver = NexYamlSerializerRegistry.Create(typeof(Collections).Assembly);
+
+    }
+    [IterationSetup]
+    public void Init()
+    {
+        s = new StreamWriter(memoryStream = new MemoryStream(4000));
+        writer = new YamlWriter(s,resolver,IResolvePlugin.plugins);
+
+    }
+    [Benchmark(Baseline = true)]
+    public void YamlB()
+    {
+        writer.Write(values,DataStyle.Compact);
+     
+        s.Flush();
+
+    }
+    [Benchmark()]
+    public void JsonB()
+    {
+        var x = JsonSerializer.Serialize(values, MyJsonContext.Default.Collections);
+    }
 }
 [DataContract]
 internal class Collections
 {
     [JsonInclude]
-    public List<TempData> values = new();
-
-    public static void Collectio()
+    public TempData values = new TempData
     {
-        List<TempData> values = new();
-        for (var i = 0; i < 100000; i++)
-        {
-            values.Add(new TempData { Name = "John", Id = 1 });
-        }
-        var reg = NexYamlSerializerRegistry.Create(typeof(Collections).Assembly);
-        new ListSerializerFactory().Register(reg);
-        using var mem = new MemoryStream();
-        using var StreamWriter = new StreamWriter(mem);
-        Yaml.Write(values, StreamWriter, DataStyle.Any,reg);
-    }
-    public void Bench()
+        Id = 2,
+        Name = $"Name_1",
+    };
+
+    public Collections()
     {
 
     }
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -29,29 +29,7 @@ class Program
 
     static void Main()
     {
-        NexYamlSerializerRegistry.Init();
-        // Calculate how many structs are needed to reach approximately 500MB
-        const int structSize = 24;  // Approximate size of each struct in bytes
-        const int targetSizeMB = 500;
-
-        int numberOfStructs = (targetSizeMB * 1024 * 1024) / structSize;
-
-        // Create a large list of structs
-        var dataList = new List<Data>(10000000);
-        for (int i = 0; i < 10000000; i++)
-        {
-            dataList.Add(new Data { Id = i, Value = "Value " + i });
-        }
-        var wrapper = new Wrapper();
-        wrapper.Data = dataList.ToArray();
-        using FileStream stream = new FileStream("D:\\output.json", FileMode.Open);
-        using StreamWriter writer = new StreamWriter(stream);
-        // Measure the time taken to serialize the data
-        Stopwatch stopwatch = Stopwatch.StartNew();
-        Yaml.Write(wrapper,writer);
-        stopwatch.Stop();
-        Console.WriteLine($"Serialization Time: {stopwatch.ElapsedMilliseconds} ms");
-
-        // Optionally, write to a file to see the JSON output
+        // Yaml.Write(new Collections() { });
+        BenchmarkRunner.Run<Benchmarker>();
     }
 }
