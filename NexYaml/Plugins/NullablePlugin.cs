e44517c3773a12b695c179a767db60415d6b8578
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-05-16 20:55:00 +0200

    merge

--- NexYaml/Plugins/NullablePlugin.cs
+++ NexYaml/Plugins/NullablePlugin.cs
@@ -13,21 +13,13 @@ internal class NullablePlugin : IResolvePlugin
         return false;
     }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 1d072fd9cf40e0531369f2ee24bfbf904c91a917
     public bool Read<T>(IYamlReader stream, T value, ParseContext<T> result)
     {
         return false;
     }
 
-<<<<<<< HEAD
     public bool Write<T, X>(WriteContext<X> context, T value, DataStyle style)
         where X : Node
-=======
-    public bool Write<T>(IYamlWriter stream, T value, DataStyle style, WriteContext context, out WriteContext newContext)
->>>>>>> 1d072fd9cf40e0531369f2ee24bfbf904c91a917
     {
         var type = typeof(T);
         Type? targetType = null;
