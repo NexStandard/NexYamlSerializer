ddd8d95bbe37513ee0ab098ea4e3c08ffc213b01
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-05-16 16:50:59 +0200

    feat: add scalar tests

---
+++ NexYamlTest/ScalarStyle/ScalarTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NexYaml;
+using Xunit;
+
+namespace NexYamlTest.ScalarStyle
+{
+    public class ScalarTests
+    {
+        [Fact]
+        public void PlainScalar()
+        {
+            NexYamlSerializerRegistry.Init();
+            var w = new StringWrapper()
+            {
+                Value = "ScalarStylePlainScalar",
+            };
+            var s = Yaml.Write(w);
+            Assert.Equal("!NexYamlTest.ScalarStyle.StringWrapper,NexYamlTest\nValue: ScalarStylePlainScalar", s);
+
+        }
+        [Fact]
+        public void DoubleQuotedScalar()
+        {
+            NexYamlSerializerRegistry.Init();
+            var w = new StringWrapper()
+            {
+                Value = "!{[ ] , # ` \" \' &*?|-><=%@. "
+            }
+            ;
+            var s = Yaml.Write(w);
+            Assert.Contains("\"!{[ ] , # ` \" ' &*?|-><=%@. \"", s);
+        }
+        [Fact]
+        public void LiteralScalar()
+        {
+            NexYamlSerializerRegistry.Init();
+            var w = new StringWrapper()
+            {
+                Value = "\n\n!{[ ] \n, # ` \" \' &*?|-><=%@."
+            }
+            ;
+            var s = Yaml.Write(w);
+            Assert.Contains("|+", s);
+        }
+    }
+}
---
+++ NexYamlTest/ScalarStyle/StringWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core;
+
+namespace NexYamlTest.ScalarStyle
+{
+    [DataContract]
+    internal class StringWrapper
+    {
+        public string Value { get; set; }
+    }
+}
