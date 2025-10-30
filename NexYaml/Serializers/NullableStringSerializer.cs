9bea903b93b56ebff4bf60c61cc4ba86d01aa50b
Eideren <contact@eideren.com>
2025-10-30 19:26:57 +0100

    Clarify NullableStringSerializer

--- NexYaml/Serializers/NullableStringSerializer.cs
+++ NexYaml/Serializers/NullableStringSerializer.cs
@@ -9,19 +9,16 @@ public class NullableStringSerializer : IYamlSerializer<string?>
 {
     public void Write<X>(WriteContext<X> context, string? value, DataStyle style) where X : Node
     {
-        // Is this correct ? -Eideren
-        if (value is null)
-            context.WriteScalar(YamlCodes.Null);
-        else
-            context.WriteScalar(context.Writer.FormatString(context, value, style));
+        // Should be taken care of in the caller's scope, which would be Writer.WriteType
+        System.Diagnostics.Debug.Assert(value is not null);
+        context.WriteScalar(context.Writer.FormatString(context, value, style));
     }
 
     public ValueTask<string?> Read(Scope scope, string? parseResult)
     {
         var scalarScope = scope.As<ScalarScope>();
-        // Given the changes above, is this correct, wouldn't this return a null string if the string contains "!!null" ? -Eideren
-        if (scalarScope.Value == YamlCodes.Null)
-            return default;
+        // Should be taken care of in the caller's scope, which would be ScopeExtensions.Read
+        System.Diagnostics.Debug.Assert(scalarScope.Value != YamlCodes.Null);
         return new(scalarScope.Value);
     }
 }
