8f354eb5c4497070f4a064bf3ee6557ac9874210
IXLLEGACYIXL <ixllegacy123@outlook.com>
2025-11-03 05:52:31 +0100

    fix: WriteContext as struct

--- NexYaml/Serialization/WriteContext.cs
+++ NexYaml/Serialization/WriteContext.cs
@@ -5,10 +5,26 @@ namespace NexYaml.Serialization;
 /// <summary>
 /// The current context for writing YAML content. This record encapsulates the current state during YAML serialization,
 /// </summary>
-/// <typeparam name="T">The type of the current YAML <see cref="Node"/>.</typeparam>
-/// <param name="Indent">The current indentation level for formatting the YAML output.</param>
-/// <param name="IsRedirected">A flag indicating whether the context has been redirected (i.e., when the actual runtime type differs from the expected type).</param>
-/// <param name="StyleScope">The <see cref="DataStyle"/> (e.g., compact, normal) used for the current <see cref="Node"/>.</param>
-/// <param name="Node">The active YAML node associated with this context.</param>
-/// <param name="Writer">The <see cref="Writer"/> instance that handles the output of the YAML content.</param>
-public record WriteContext<T>(int Indent, bool IsRedirected, DataStyle StyleScope, T Node, Writer Writer) where T : Node;
+public readonly struct WriteContext<T>(int indent, bool isRedirected, DataStyle styleScope, T node, Writer writer) where T : Node
+{
+    /// <summary>
+    /// The current indentation level for formatting the YAML output.
+    /// </summary>
+    public int Indent { get; init; } = indent;
+    /// <summary>
+    /// A flag indicating whether the context has been redirected (i.e., when the actual runtime type differs from the expected type).
+    /// </summary>
+    public bool IsRedirected { get; init; } = isRedirected;
+    /// <summary>
+    /// The <see cref="DataStyle"/> (e.g., compact, normal) used for the current <see cref="Node"/>.
+    /// </summary>
+    public DataStyle StyleScope { get; init; } = styleScope;
+    /// <summary>
+    /// The active YAML node associated with this context.
+    /// </summary>
+    public T Node { get; init; } = node;
+    /// <summary>
+    /// The <see cref="Writer"/> instance that handles the output of the YAML content.
+    /// </summary>
+    public Writer Writer { get; init; } = writer;
+}
