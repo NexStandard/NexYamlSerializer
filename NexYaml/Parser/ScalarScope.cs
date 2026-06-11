909ef3c86e8194a1d7eaf9f6507428bd440d9ef1
IXLLEGACYIXL <ixllegacy123@outlook.com>
2026-06-11 21:35:17 +0200

    fix: remove impossible occurrence of a tag redirection

--- NexYaml/Parser/ScalarScope.cs
+++ NexYaml/Parser/ScalarScope.cs
@@ -27,22 +27,6 @@ namespace NexYaml.Parser
         public static Scope Parse(ScopeContext context, string val, int indent, string tag)
         {
             var valSpan = val.AsSpan();
-            if (valSpan.StartsWith('!') && val != YamlCodes.Null)
-            {
-                var spaceIdx = valSpan.IndexOf(' ');
-                var childTag = valSpan[..spaceIdx].ToString();
-
-                // Skip spaces
-                do
-                {
-                    spaceIdx++;
-                } while (spaceIdx < valSpan.Length && valSpan[spaceIdx] == ' ');
-
-                // Skip first character
-                // Replicated the base logic, but not sure why we would skip the first character of what's after the space though -Eideren
-                string rest = spaceIdx + 1 < val.Length ? valSpan[(spaceIdx + 1)..].ToString() : string.Empty;
-                return Parse(context, rest, indent, childTag);
-            }
 
             if (TryGetQuotedText(valSpan, out var unquotedSpan))
                 return new ScalarScope(unquotedSpan.ToString(), indent, context, tag);
